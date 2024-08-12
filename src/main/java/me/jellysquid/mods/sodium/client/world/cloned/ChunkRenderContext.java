79b0f3d5636f4fadc8dfa9b8dd76fc5c8c73c8a1
mik3se <mik3se@gmail.com>
2024-08-11 20:19:05 -0700

    Fix Java 8 compatibility

--- src/main/java/me/jellysquid/mods/sodium/client/world/cloned/ChunkRenderContext.java
+++ src/main/java/me/jellysquid/mods/sodium/client/world/cloned/ChunkRenderContext.java
@@ -3,7 +3,37 @@ package me.jellysquid.mods.sodium.client.world.cloned;
 import me.jellysquid.mods.sodium.client.util.math.ChunkSectionPos;
 import net.minecraft.util.math.BlockBox;
 
-public record ChunkRenderContext(ChunkSectionPos origin, ClonedChunkSection[] sections, BlockBox volume) {
+/*public record ChunkRenderContext(ChunkSectionPos origin, ClonedChunkSection[] sections, BlockBox volume) {
+    public void releaseResources() {
+        for (ClonedChunkSection section : sections) {
+            if (section != null)
+                section.getBackingCache().release(section);
+        }
+    }
+}*/
+public final class ChunkRenderContext{
+    ChunkSectionPos origin;
+    ClonedChunkSection[] sections;
+    BlockBox volume;
+
+    public ChunkRenderContext(ChunkSectionPos origin, ClonedChunkSection[] sections, BlockBox volume) {
+        this.origin = origin;
+        this.sections = sections;
+        this.volume = volume;
+    }
+
+    public ChunkSectionPos origin() {
+        return origin;
+    }
+
+    public ClonedChunkSection[] sections() {
+        return sections;
+    }
+
+    public BlockBox volume() {
+        return volume;
+    }
+
     public void releaseResources() {
         for (ClonedChunkSection section : sections) {
             if (section != null)
